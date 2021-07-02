0e46c92cf67973d5b0110ec5b2f1a7195cc91cf7
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-07-02 13:52:39 -0500

    fixed else statement

--- models.py
+++ models.py
@@ -8,8 +8,7 @@ if 'ON_HEROKU' in os.environ:
                                   host='ec2-52-0-67-144.compute-1.amazonaws.com', port=5432)
 
 else:
-    DATABASE = PostgresqlDatabase('d8f9bkplq8552n', user='ukyriqaoykvzfy', password='ad8142353c61b8181a2be5fd1fd87afd5694be43b2557df525612310a32e87c2',
-                                  host='ec2-52-0-67-144.compute-1.amazonaws.com', port=5432)
+    DATABASE = PostgresqlDatabase('whispering_oaks')
 
 
 class Visitor(Model):
