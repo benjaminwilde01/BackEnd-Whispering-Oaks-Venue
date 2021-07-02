6c98d5ffa12a468159dc65009d33587ed30857ff
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-07-02 13:34:02 -0500

    added heroku psql

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from peewee import *
 from playhouse.db_url import connect
 
 if 'ON_HEROKU' in os.environ:
-    DATABASE = connect(os.environ.get(
-        'postgres: // ukyriqaoykvzfy: ad8142353c61b8181a2be5fd1fd87afd5694be43b2557df525612310a32e87c2@ec2-52-0-67-144.compute-1.amazonaws.com: 5432/d8f9bkplq8552n'))
+    DATABASE = PostgresqlDatabase('d8f9bkplq8552n', user='ukyriqaoykvzfy', password='ad8142353c61b8181a2be5fd1fd87afd5694be43b2557df525612310a32e87c2',
+                                  host='ec2-52-0-67-144.compute-1.amazonaws.com', port=5432)
 
 else:
     DATABASE = PostgresqlDatabase('whispering_oaks')
