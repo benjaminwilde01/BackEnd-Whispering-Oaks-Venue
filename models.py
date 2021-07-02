3085a575a2d48013a931f676ec6915ae95fac0dd
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-07-02 13:40:05 -0500

    testing db

--- models.py
+++ models.py
@@ -8,7 +8,8 @@ if 'ON_HEROKU' in os.environ:
                                   host='ec2-52-0-67-144.compute-1.amazonaws.com', port=5432)
 
 else:
-    DATABASE = PostgresqlDatabase('whispering_oaks')
+    DATABASE = PostgresqlDatabase('d8f9bkplq8552n', user='ukyriqaoykvzfy', password='ad8142353c61b8181a2be5fd1fd87afd5694be43b2557df525612310a32e87c2',
+                                  host='ec2-52-0-67-144.compute-1.amazonaws.com', port=5432)
 
 
 class Visitor(Model):
